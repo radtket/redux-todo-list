35dce8d0c3dfdd22fd97503233d7679a086e83b4
Taylor Radtke <taylorradtke@gmail.com>
2025-01-01 15:31:00 -0600

    Updates

---
+++ precache-manifest.b4aac61179eb0077541b4d1cfbd35a37.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44fcd30c285117db366e71a1fb73cdf8",
+    "url": "/redux-todo-list/index.html"
+  },
+  {
+    "revision": "da70760518bf9a1c5a50",
+    "url": "/redux-todo-list/static/css/main.004fcc88.chunk.css"
+  },
+  {
+    "revision": "dffe9cd82b8c05ef87ce",
+    "url": "/redux-todo-list/static/js/2.fb249f9b.chunk.js"
+  },
+  {
+    "revision": "2d0dc3ea56ea29aa89323d664321ba6d",
+    "url": "/redux-todo-list/static/js/2.fb249f9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da70760518bf9a1c5a50",
+    "url": "/redux-todo-list/static/js/main.3a45240b.chunk.js"
+  },
+  {
+    "revision": "ab1eadd44d0398283f41",
+    "url": "/redux-todo-list/static/js/runtime-main.dd160f13.js"
+  }
+]);
